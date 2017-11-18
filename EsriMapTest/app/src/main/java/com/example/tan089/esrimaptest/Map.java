b2a24e1f93e234af62c24d71f17e0f26f7524a28
tando89 <tantdo89@gmail.com>
2017-11-18 13:10:42 -0800

    map testing

---
+++ EsriMapTest/app/src/main/java/com/example/tan089/esrimaptest/Map.java
@@ -0,0 +1,105 @@
+package com.example.tan089.esrimaptest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReference;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class Map extends AppCompatActivity {
+    private MapView mMapView;
+    private LocationDisplay mLocationDisplay;
+    private static final int SCALE = 1500;
+    private SpatialReference spatialReference;
+    private int requestCode = 2;
+    String[] reqPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission
+            .ACCESS_COARSE_LOCATION};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        mMapView = (MapView) findViewById(R.id.myMap);
+        ArcGISMap mMap = new ArcGISMap(Basemap.createImagery());
+        mMapView.setMap(mMap);
+        spatialReference = SpatialReference.create(102100);
+        //testing long and lat of CSUSB
+        Point sbPoint = new Point(-13060292.835, 4053300.978, spatialReference);
+        Viewpoint viewpoint = new Viewpoint(sbPoint, SCALE);
+        mMapView.setViewpointCenterAsync(sbPoint, SCALE);
+        // get the MapView's LocationDisplay
+        mLocationDisplay = mMapView.getLocationDisplay();
+
+        mLocationDisplay.addDataSourceStatusChangedListener(new LocationDisplay.DataSourceStatusChangedListener() {
+            @Override
+            public void onStatusChanged(LocationDisplay.DataSourceStatusChangedEvent dataSourceStatusChangedEvent) {
+
+                // If LocationDisplay started OK, then continue.
+                if (dataSourceStatusChangedEvent.isStarted())
+                    return;
+
+                // No error is reported, then continue.
+                if (dataSourceStatusChangedEvent.getError() == null)
+                    return;
+
+                // If an error is found, handle the failure to start.
+                // Check permissions to see if failure may be due to lack of permissions.
+                boolean permissionCheck1 = ContextCompat.checkSelfPermission(Map.this, reqPermissions[0]) ==
+                        PackageManager.PERMISSION_GRANTED;
+                boolean permissionCheck2 = ContextCompat.checkSelfPermission(Map.this, reqPermissions[1]) ==
+                        PackageManager.PERMISSION_GRANTED;
+
+                if (!(permissionCheck1 && permissionCheck2)) {
+                    // If permissions are not already granted, request permission from the user.
+                    ActivityCompat.requestPermissions(Map.this, reqPermissions, requestCode);
+                } else {
+                    // Report other unknown failure types to the user - for example, location services may not
+                    // be enabled on the device.
+                    String message = String.format("Error in DataSourceStatusChangedListener: %s", dataSourceStatusChangedEvent
+                            .getSource().getLocationDataSource().getError().getMessage());
+                    Toast.makeText(Map.this, message, Toast.LENGTH_LONG).show();
+
+                    // Update UI to reflect that the location display did not actually start
+                }
+            }
+        });
+        if (!mLocationDisplay.isStarted())
+            mLocationDisplay.startAsync();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Location permission was granted. This would have been triggered in response to failing to start the
+            // LocationDisplay, so try starting this again.
+            mLocationDisplay.startAsync();
+        } else {
+            // If permission was denied, show toast to inform user what was chosen. If LocationDisplay is started again,
+            // request permission UX will be shown again, option should be shown to allow never showing the UX again.
+            // Alternative would be to disable functionality so request is not shown again.
+            Toast.makeText(getApplicationContext(), "Please allow the location service", Toast.LENGTH_LONG).show();
+
+        }
+    }
+    @Override
+    protected void onPause(){
+        mMapView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
+    }
+}
