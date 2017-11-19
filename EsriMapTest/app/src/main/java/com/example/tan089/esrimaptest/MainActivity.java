9eb8911fbd5a671c70047f6343fee6d4331d33e5
tando89 <tantdo89@gmail.com>
2017-11-18 18:40:08 -0800

    added 2nd map

--- EsriMapTest/app/src/main/java/com/example/tan089/esrimaptest/MainActivity.java
+++ EsriMapTest/app/src/main/java/com/example/tan089/esrimaptest/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button MapBnt;
+    private Button MapBnt, Map2Bnt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,12 +15,21 @@ public class MainActivity extends AppCompatActivity {
 
         MapBnt = (Button) findViewById(R.id.mapActivity);
 
+        Map2Bnt = (Button) findViewById(R.id.map2Activity);
+
         MapBnt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Map.class);
                 startActivity(intent);
             }
+        });
+        Map2Bnt.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent(MainActivity.this, Map2Activity.class);
+                 startActivity(intent);
+             }
         });
     }
 }
